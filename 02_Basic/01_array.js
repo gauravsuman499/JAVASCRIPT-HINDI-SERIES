cc8f5a2ccf446e94f9d34933fbe77f3eae75666a
gauravsuman499 <116598523+gauravsuman499@users.noreply.github.com>
2024-12-04 13:51:05 +0000

    array part1

---
+++ 02_Basic/01_array.js
@@ -0,0 +1,50 @@
+// arrays
+
+
+const num = [1, 2,3, 4, 5, 6] ;
+
+/*
+//push & pop methods 
+num.push(7) ; //insert elemnet into the end of the array
+num.push(8) ;
+num.push(9) ;
+
+console.log(num) ;
+
+num.pop() ; //removes element from the end of the array
+console.log(num) ;
+
+
+
+//shift & unshift methods
+num.unshift(9) ; // insert value into the start (time consuming)
+console.log(num) ;
+
+num.shift() ;//remove array elements from the start
+console.log(num) ;
+
+//check methods of the array
+
+console.log(num.includes(12));
+console.log(num.includes(1));
+console.log(num.indexOf(4));
+
+//slice & splice functions
+const slicedArray = num.slice(1,3) ; // returns a new separated array part from the original array 
+console.log(slicedArray);
+console.log(num) ; //no change to the original array
+
+
+const splicedArray = num.splice(1,3) ;
+console.log(splicedArray); //return a new separated array part from the original array 
+console.log(num);//changes the origianl array (all the spliced part is removed)
+
+*/
+
+
+//join operation on array
+const joinArry = num.join() ; //copnnverts arrays into strings format(comma format) 
+console.log(joinArry);
+
+
+
