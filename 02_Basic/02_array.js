5d465bbc16a778e40aeba0bfff58d243bb529c78
gauravsuman499 <116598523+gauravsuman499@users.noreply.github.com>
2024-12-04 14:31:09 +0000

    array basic 2

---
+++ 02_Basic/02_array.js
@@ -0,0 +1,32 @@
+const  heros = ["nagraj", "Doga", "superman"];
+const heros2 = ["Spiderman", "captain","ironMan"] ;
+
+/*
+// heros.push(heros2) ;
+const newHeros = heros.concat(heros2); /// return a new array containing all rthe elemnts from both the arrays
+console.log(newHeros);
+
+const newHeros2 = [...heros, ...heros2] ;
+console.log(newHeros2);
+
+
+//flat method of array
+const arry = [1,2,3,[4,5,6,[7,8,9,[19, 20]]]] ;
+console.log(arry.flat(5));
+
+
+//conversion form other data structures to array
+const nArry = Array.isArray("Gaurav") ;
+console.log(nArry);
+
+console.log(Array.from("Suman")) ;
+
+const score1 = 100;
+const score2 = 200;
+const score3 = 300;
+
+const nArray = Array.of(score1, score2, score3);
+console.log(nArray);
+*/
+
+
