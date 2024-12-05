d126ebdc1beccdec9ace86226997c347851aa7fa
gauravsuman499 <116598523+gauravsuman499@users.noreply.github.com>
2024-12-05 09:48:55 +0000

    objects basic 02

---
+++ 02_Basic/04_objects.js
@@ -0,0 +1,55 @@
+const tinderUser = {} ; // empty objects
+
+tinderUser.id = 4471;
+tinderUser.name = "Gaurav Suman";
+tinderUser.isLoggedIn = true ;
+
+// console.log(tinderUser);
+
+
+const regularUser = {
+    email : " xyz@gmail.com",
+    fullname : {
+        userFullName : {
+            fisrtName : "gaurav",
+            lastName : "Suman"
+        }
+    }
+};
+
+console.log(regularUser.fullname.userFullName.fisrtName);
+console.log(regularUser.fullname.userFullName.lastName);
+
+
+const obj1 = {
+    1 : "a",
+    2 : "b"
+};
+
+const obj2 = {
+    3 : "a",
+    4 : "b"
+};
+
+// const obj3 = {obj1, obj2} ;
+// const obj3 = Object.assign({}, obj1, obj2) ; //assign method to join all the objects together
+const obj3 = {...obj1, ...obj2} ; // ******* spread oprerator to join all the objects together. *******
+
+console.log(obj3);
+
+//array of objects
+/*
+const users = [ 
+    {},
+    {},
+    {},
+    {}
+]
+
+*/
+
+console.log(Object.keys(tinderUser)); //return an array of all the keys of the object
+console.log(Object.values(tinderUser)); //return an array of all the values of the object
+console.log(Object.entries(tinderUser)); //return an array of all the enteries of the object
+
+console.log(tinderUser.hasOwnProperty("name"));
