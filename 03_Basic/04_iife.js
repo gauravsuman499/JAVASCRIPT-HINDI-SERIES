2382073bce57c0ed585adc3c08224cfac89ee059
gauravsuman499 <116598523+gauravsuman499@users.noreply.github.com>
2024-12-10 13:58:18 +0000

    iife function

---
+++ 03_Basic/04_iife.js
@@ -0,0 +1,13 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})();
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+} )('hitesh')
+
+// note: always use semicolon at the end of a iife function(as iiffe function doesnt get terminated by self);
